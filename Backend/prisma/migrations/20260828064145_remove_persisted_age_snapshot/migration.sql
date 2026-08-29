f9326e7a34ac1ed8a8def944a8b39838d6f4f93b
Anandu <ananduak472k20@gmail.com>
2026-08-29 22:36:08 +0530

    Revert "Patient Implementation" This reverts commit 9c57ea70f6807a0e57be0b6d31df58304e017616.

--- Backend/prisma/migrations/20260828064145_remove_persisted_age_snapshot/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `ageAtVisit` on the `PatientVisit` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "PatientVisit" DROP COLUMN "ageAtVisit";
